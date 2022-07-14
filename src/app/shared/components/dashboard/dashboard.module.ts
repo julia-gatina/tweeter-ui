39108e15637ed8888d71506bc3114f90cc42b588
Julia <julia.sarandi@gmail.com>
2022-07-14 00:47:54 -0700

    Create new profile component.

--- src/app/shared/components/dashboard/dashboard.module.ts
+++ src/app/shared/components/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [DashboardComponent, NavigationComponent],
+  declarations: [DashboardComponent, NavigationComponent, ProfileComponent],
   imports: [CommonModule],
   exports: [DashboardComponent],
 })
