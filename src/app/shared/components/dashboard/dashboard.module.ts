15531a3668a6cd7461ab612f34a16a99acdecc3c
Julia <julia.sarandi@gmail.com>
2022-07-14 00:51:34 -0700

    Create new tweet-card component.

--- src/app/shared/components/dashboard/dashboard.module.ts
+++ src/app/shared/components/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { ProfileComponent } from './profile/profile.component';
+import { TweetCardComponent } from './tweet-card/tweet-card.component';
 
 @NgModule({
-  declarations: [DashboardComponent, NavigationComponent, ProfileComponent],
+  declarations: [DashboardComponent, NavigationComponent, ProfileComponent, TweetCardComponent],
   imports: [CommonModule],
   exports: [DashboardComponent],
 })
